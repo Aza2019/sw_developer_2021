fe1a024a2aabab9c332d7a1dc30bad8cf05e10d1
Franc@Wifi <vincentjb07@hotmail.com>
2021-10-12 21:35:34 +0200

    DataTypes

---
+++ src/12102021/arithmethische_OP/arithmethische_OP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmethische_OP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arithmetische Operatoren
+            //+-*/
+            //%=Restwertdivision(Modulo)
+
+           //int z1 = 0;
+           // int z2 = 0;
+           // int z3 = 0;
+
+            //z3 = z1 + 654.22);
+
+            //vergleichs-Operatoren
+            //<> >= <=
+            //== !=
+
+           //binäre Operatoren
+           //& | && || !
+
+            //zusammengesetzte Operatoren
+
+        }
+    }
+}
