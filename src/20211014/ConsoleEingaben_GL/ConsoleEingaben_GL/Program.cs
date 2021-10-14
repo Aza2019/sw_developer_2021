0ad054ad486ffa22d0deb4ded219ca018bbd83f1
Franc@Wifi <vincentjb07@hotmail.com>
2021-10-14 21:26:24 +0200

    Consol EIngabe

---
+++ src/20211014/ConsoleEingaben_GL/ConsoleEingaben_GL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEingaben_GL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string geburtsJahrEingabe = string.Empty;
+            int alter = 0;
+            int geburtsJahr = 0;
+
+            Console.WriteLine("Bitte Name eingeben: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("\nHallo " + name + ", wie geht`s?");
+
+            Console.WriteLine(name + ", bittte gib dein Geburtsjahr ein: ");
+            geburtsJahrEingabe = Console.ReadLine();
+
+            //Konvertierung Zeichenkette nach int
+
+            geburtsJahr = int.Parse(geburtsJahrEingabe);
+            //Alter vom User berechnen
+            alter = DateTime.Now.Year - geburtsJahr;
+
+            Console.WriteLine(name + ", du bist " + alter + " Jahre alt.");
+        }
+    }
+}
---
+++ src/20211014/Exceiton_GL/Exception_GL/Exception_GL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_GL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int geburtsJahr = 0;
+            int alter = 0;
+            bool isInputValid = false;
+
+            Console.Write("Bitte Geburtsjahr eingeben: ");
+
+            try
+            {
+              
+                geburtsJahr = int.Parse(Console.ReadLine());
+                isInputValid = true;
+
+               
+            }
+            catch
+            {
+                isInputValid = true;
+
+                Console.WriteLine("Unfortunately something went wrong!");
+            }
+            if (isInputValid == true)
+            {
+
+                alter = DateTime.Now.Year - geburtsJahr;
+                Console.WriteLine("Du bist " + alter + " Jahre alt. ");
+
+   
+            }
+        }
+    }
+}
