50fdab3e308c5491416b4eb08dda2feebebbc5ea
Franc@Wifi <vincentjb07@hotmail.com>
2021-10-14 18:25:21 +0200

    Altogether Oparation

--- src/12102021/arithmethische_OP/arithmethische_OP/Program.cs
+++ src/12102021/arithmethische_OP/arithmethische_OP/Program.cs
@@ -14,9 +14,9 @@ namespace arithmethische_OP
             //+-*/
             //%=Restwertdivision(Modulo)
 
-           //int z1 = 0;
-           // int z2 = 0;
-           // int z3 = 0;
+            //int z1 = 0;
+            // int z2 = 0;
+            // int z3 = 0;
 
             //z3 = z1 + 654.22);
 
@@ -24,11 +24,27 @@ namespace arithmethische_OP
             //<> >= <=
             //== !=
 
-           //binäre Operatoren
-           //& | && || !
+            //binäre Operatoren
+            //& | && || !
 
             //zusammengesetzte Operatoren
+            int einZAhl = 5;
 
+            einZAhl = einZAhl + 5;
+            eineZahl += 5;
+            einZAhl -= 8;
+            einZahl *= 2;
+            einZAhl /= 3;
+
+            einZAhl += 1;
+            
+            //inkrementieren
+            einZAhl++;
+
+            //dekrementieren
+            einZAhl--;
+
+                
         }
     }
 }
